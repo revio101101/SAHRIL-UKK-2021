725662222ec5ff51270dd4a3f49904794e3f7e04
revio101101 <81915270+revio101101@users.noreply.github.com>
2021-04-10 02:04:24 +0700

    Update print_report.php

--- view/admin/print_report.php
+++ view/admin/print_report.php
@@ -34,245 +34,71 @@ auth_petugas();
         <!-- Begin Page Content -->
         <div class="mt-4 container-fluid">
 
-          <!-- Page Heading -->
-          <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-grey-800">Data dan Grafik Pengaduan</h1>
-            <button onclick="window.print();" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-print fa-sm text-white-50"></i> Cetak Laporan</button>
-          </div>
-
-          <!-- Content Row -->
-          <div class="row">
-
-<?php
-  $proses = mysqli_num_rows(mysqli_query($koneksi, "SELECT * FROM pengaduan WHERE status='proses'"));
-  $belumditanggapi = mysqli_num_rows(mysqli_query($koneksi, "SELECT * FROM pengaduan WHERE status='0'"));
-  $selesai = mysqli_num_rows(mysqli_query($koneksi, "SELECT * FROM pengaduan WHERE status='selesai'"));
-  $jumlah = mysqli_num_rows(mysqli_query($koneksi, "SELECT * FROM pengaduan"));
-  if(empty($jumlah)){
-    error_reporting(0);
+  <body>
+<style type="text/css">
+.head {
+  width:100%;
+  height:100px;
+  border-bottom:1px solid;
   }
-?>
-
-            <!-- Pending Requests Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card border-left-warning shadow h-100 py-2">
-                <div class="card-body">
-                  <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Belum Ditanggapi</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $belumditanggapi;?></div>
-                    </div>
-                    <div class="col-auto">
-                      <i class="fas fa-clock fa-2x text-gray-300"></i>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <!-- Earnings (Monthly) Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                  <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Pengaduan Diproses</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $proses;?></div>
-                    </div>
-                    <div class="col-auto">
-                      <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <!-- Earnings (Monthly) Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card border-left-success shadow h-100 py-2">
-                <div class="card-body">
-                  <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Pengaduan Selesai</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $selesai;?></div>
-                    </div>
-                    <div class="col-auto">
-                      <i class="fas fa-check fa-2x text-gray-300"></i>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <!-- Earnings (Monthly) Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card border-left-info shadow h-100 py-2">
-                <div class="card-body">
-                  <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Jumlah Pengaduan</div>
-                      <div class="row no-gutters align-items-center">
-                        <div class="col-auto">
-                          <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?= $jumlah;?></div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="col-auto">
-                      <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <!-- Content Row -->
-          <div class="row">
-            <!-- Content Column -->
-            <div class="col-xl-8 col-lg-7">
-
-              <!-- Project Card Example -->
+</style>
+<div class="head">
+<table>
+<tr>
+<td width="10%"><img src="../img/logo.png" width="90px" /></td>
+<td align="center" width="90%"><font style="font-size:22px; font-family:Arial, Helvetica, sans-serif;">PEMERINTAH KABUPATEN BANDUNG BARAT<br />KECAMATAN BATUJAJAR<br/>DESA GALANGGANG</font><br /><i><font style="font-size:15px;"></font></i></td>
+</tr>
+</table>
+</div><br /><br />
+<div style="font-size:24px; text-align:center;">Laporan Pengaduan Masyarakat Desa Galanggang</div><br /><br />
+<table align="center" border="1" cellpadding="8" cellspacing="0">
+<tr>
+      <th>No</th>
+      <th>Tgl Pengaduan</th>
+      <th>NIK</th>
+      <th>Isi Laporan</th>
+            <th>Foto</th>
+            <th>Status</th>
+            <th>Tanggapan</th>
+    </tr>
 <?php
- $persen = 100;
- $persenproses = $proses / $jumlah * $persen;
- $persenbelumditanggapi = $belumditanggapi / $jumlah * $persen;
- $persenselesai = $selesai / $jumlah * $persen;
+  $out = mysqli_query($koneksi, "SELECT * FROM tanggapan");
+  while($keluar = mysqli_fetch_array($out)){
 ?>
-              <div class="card shadow mb-4">
-                <div class="card-header py-3">
-                  <h6 class="m-0 font-weight-bold text-primary">Persentase Pengaduan</h6>
-                </div>
-                <div class="card-body">
-                  <h4 class="small font-weight-bold">Belum Ditanggapi <span class="float-right"><?= $persenbelumditanggapi."%";?></span></h4>
-                  <div class="progress mb-4">
-                    <div class="progress-bar bg-warning" role="progressbar" style="width: <?= $persenbelumditanggapi."%";?>" aria-valuenow="<?= $persenbelumditanggapi."%";?>" aria-valuemin="0" aria-valuemax="100"></div>
-                  </div>
-                  <h4 class="small font-weight-bold">Sedang Diproses <span class="float-right"><?= $persenproses."%";?></span></h4>
-                  <div class="progress mb-4">
-                    <div class="progress-bar" role="progressbar" style="width: <?= $persenproses."%";?>" aria-valuenow="<?= $persenproses."%";?>" aria-valuemin="0" aria-valuemax="100"></div>
-                  </div>
-                  <h4 class="small font-weight-bold">Pengaduan Selesai <span class="float-right"><?= $persenselesai."%";?></span></h4>
-                  <div class="progress">
-                    <div class="progress-bar bg-success" role="progressbar" style="width: <?= $persenselesai."%";?>" aria-valuenow="<?= $persenselesai."%";?>" aria-valuemin="0" aria-valuemax="100"></div>
-                  </div>
-                </div>
-              </div>   
-            </div>
-            <!-- Pie Chart -->
-            <div class="col-xl-4 col-lg-5">
-              <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                  <h6 class="m-0 font-weight-bold text-primary">Grafik Lingkaran</h6>
-                  <div class="dropdown no-arrow">
-                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-                      <div class="dropdown-header">Dropdown Header:</div>
-                      <a class="dropdown-item" href="#">Action</a>
-                      <a class="dropdown-item" href="#">Another action</a>
-                      <div class="dropdown-divider"></div>
-                      <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                  </div>
-                </div>
-                <!-- Card Body -->
-                <div class="card-body">
-                  <div class="chart-pie pt-4 pb-2">
-                    <canvas id="bulet"></canvas>
-                  </div>
-                  <div class="mt-4 text-center small">
-                    <span class="mr-2">
-                      <i class="fas fa-circle text-primary"></i> Diproses
-                    </span>
-                    <span class="mr-2">
-                      <i class="fas fa-circle text-success"></i> Selesai
-                    </span>
-                    <span class="mr-2">
-                      <i class="fas fa-circle text-warning"></i> Belum ditanggapi
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <!-- /.container-fluid -->
 
-      </div>
-      <!-- End of Main Content -->
+                    <tr>
+                      <td><?php echo $keluar['id_pengaduan'];?></td>
+                      <td><?php echo $keluar['tgl_pengaduan'];?></td>
+                      <td><?php echo $keluar['nik'];?></td>
+                      <td><?php echo $keluar['isi_laporan'];?></td>
+                      <td align="Center"><img src="../../file_upload/<?php echo $keluar['foto'];?>" style="width: 100px;height: auto;"></td>
+                      <td><p class="text-success"><b>SELESAI</b></p></td>
+                    </tr>
+                    <td><?php echo $keluar['tanggapan'];?></td>
 
-      <!-- Footer -->
-      <footer class="sticky-footer bg-white">
-        <div class="container my-auto">
-          <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Sahril Maulana</span>
-          </div>
-        </div>
-      </footer>
-      <!-- End of Footer -->
-
-    </div>
-    <!-- End of Content Wrapper -->
-
-  </div>
-  <!-- End of Page Wrapper -->
-
-  <!-- Bootstrap core JavaScript-->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  <!-- Core plugin JavaScript-->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-  <!-- Custom scripts for all pages-->
-  <script src="js/sb-admin-2.min.js"></script>
-
-  <!-- Page level plugins -->
-  <script src="vendor/chart.js/Chart.min.js"></script>
-
-  <!-- Page level custom scripts -->
-  <script src="js/demo/chart-area-demo.js"></script>
-  <script src="js/demo/chart-pie-demo.js"></script>
-  <script type="text/javascript">
-  Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-  Chart.defaults.global.defaultFontColor = '#858796';
-
-  // Pie Chart Example
-  var ctx = document.getElementById("bulet");
-  
-  var myPieChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-      labels: ["Diproses", "Selesai", "Belum ditanggapi"],
-      datasets: [{
-        data: [<?= $persenproses;?>, <?= $persenselesai;?>, <?= $persenbelumditanggapi;?>],
-        backgroundColor: ['#4e73df', '#1cc88a', '#f6c23e'],
-        hoverBackgroundColor: ['#2e59d9', '#17a673', '#f6c25e'],
-        hoverBorderColor: "rgba(234, 236, 244, 1)",
-      }],
-    },
-    options: {
-      maintainAspectRatio: false,
-      tooltips: {
-        backgroundColor: "rgb(255,255,255)",
-        bodyFontColor: "#858796",
-        borderColor: '#dddfeb',
-        borderWidth: 1,
-        xPadding: 15,
-        yPadding: 15,
-        displayColors: false,
-        caretPadding: 10,
-      },
-      legend: {
-        display: false
-      },
-      cutoutPercentage: 80,
-    },
-  });
-
-  </script>
+<?php
+}
+?>
+</table>
+<br/>
+<table class="titik">
+<tr>
+<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;GALANGGANG,.........................2021</td>
+</tr>
+<tr>
+<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;KEPALA DESA GALANGGANG</td>
+</tr>
+</table>
+<br/><br/><br/><br/>
+<table class="titik">
+<tr>
+<td><b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<u>SAHRIL MAULANA</u></b></td>
+</tr>
+</table>
+</center>
+<script>
+window.print() 
+</script>
 </body>
-
 </html>
+<?php ?>
